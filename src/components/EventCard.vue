e60399329a0449a34d497e13241775f9444fc57f
titusfrezer <titofrezer2018@gmail.com>
2021-12-19 19:41:02 +0300

    Lecture 3 ended

---
+++ src/components/EventCard.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="event-card">
+   <span>{{event.time}} on {{event.date}}</span>
+   <h4>{{event.title}}</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "EventCard",
+  props: {
+    event: Object
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.event-card{
+  padding:20px;
+  width:250px;
+  cursor: pointer;
+  border:1px solid
+}
+.event-card:hover{
+  transform: scale(1.01);
+  box-shadow: 0 12px 12px 0;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="events">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import EventCard from "@/components/EventCard.vue";
+
+export default {
+  name: "Home",
+  components: {
+    EventCard
+  },
+  data(){
+    return{
+      events:[
+        {
+          id:1244,
+          title:'animal welfare',
+          description:'find your feline friend',
+          time:'12 pm',
+          organizer:'mike d'
+        },
+        {
+          id:3555,
+          title:'London has fallen',
+          description:'find your feline friend',
+          time:'12 pm',
+          organizer:'titus d'
+        },
+        {
+          id:645454,
+          title:'Tuer welfare',
+          description:'find your feline friend',
+          time:'12 am',
+          organizer:'bisre d'
+        }
+      ]
+    }
+  }
+};
+</script>
+
+<style scoped>
+.events{
+  display:flex;
+  flex-direction: column;
+  align-items:center
+}
+</style>
